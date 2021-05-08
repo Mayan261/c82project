3351494b4c19b7147fc787095439d2712a1812d6
Mayan261 <82138988+Mayan261@users.noreply.github.com>
2021-05-08 19:46:50 +1000

    Add files via upload

---
+++ main.js
@@ -0,0 +1,32 @@
+canvas = document.getElementById("myCanvas");
+ctx = canvas.getContext("2d");
+
+var color;
+var radius;
+var width_of_line;
+canvas.addEventListener("mousedown", my_mousedown);
+
+function my_mousedown(e) {
+    
+    color = document.getElementById("color").value;
+    width_of_line = document.getElementById("width_of_line").value;
+    radius = document.getElementById("Radius").value;
+    mouseEvent = "mouseDown";
+}
+
+function mouseMove(e) {
+
+    current_position_of_mouse_x = e.clientX - canvas.offsetLeft;
+    current_position_of_mouse_y = e.clientY - canvas.offsetTop;
+
+    if(mouseEvent == "mouseDown") {
+        console.log("Current position of x and y coordinates = ");
+        console.log("x = " + current_position_of_mouse_x + "y = " + current_position_of_mouse_y);
+
+        ctx.beginPath();
+        ctx.strokeStyle = color;
+        ctx.lineWidth = width_of_line;
+        ctx.arc(current_position_of_mouse_x, current_position_of_mouse_y, radius,0 ,2 * Math.PI);
+        ctx.stroke();
+    }
+}
